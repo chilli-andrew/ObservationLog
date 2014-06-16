19e4fa9d4ffec7b247e91adff723ac225210e27d
Andrew Russell <agr001@hotmail.com>
2014-06-16 12:55:48 +0200

    Worked on moving code from behind forms into ViewModels

---
+++ source/Rusty.ObservationLog.Windows/ViewModels/ObservationViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Rusty.ObservationLog.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rusty.ObservationLog.Windows.ViewModels
+{
+    public class ObservationViewModel : ViewModelBase<ObservationViewModel>
+    {
+        private User _currentUser;
+        public User CurrentUser
+        {
+            get { return _currentUser; }
+            set { 
+                _currentUser =  value;
+                NotifyPropertyChanged<User>(o => o.CurrentUser);
+            }
+        }
+
+        private string _observationText;
+        public string ObservationText
+        {
+            get { return _observationText; }
+            set { 
+                _observationText = value;
+                NotifyPropertyChanged<string>(o => o.ObservationText);
+            }
+        }
+
+        private DateTime _observationDate;
+        public DateTime ObservationDate
+        {
+            get { return _observationDate; }
+            set { 
+                _observationDate = value;
+                NotifyPropertyChanged<DateTime>(o => o.ObservationDate);
+            }
+        }
+
+
+    }
+}
---
+++ source/Rusty.ObservationLog.Windows/ViewModels/ViewModelBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rusty.ObservationLog.Windows.ViewModels
+{
+    public class ViewModelBase<TModel> : INotifyPropertyChanged
+    {
+        // Allows you to specify a lambda for notify property changed
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool CheckPropertyChanged<TResult>(Expression<Func<TModel, TResult>> property, ref TResult oldValue, ref TResult newValue)
+        {
+            if (oldValue == null && newValue == null)
+            {
+                return false;
+            }
+
+            if ((oldValue == null && newValue != null) || !oldValue.Equals((T)newValue))
+            {
+                // Convert expression to a property name
+                string propertyName = GetPropertyName<TResult>(property);
+                oldValue = newValue;
+                InternalNotifyPropertyChanged(propertyName);
+                return true;
+            }
+
+            return false;
+        }
+
+        // Defined as virtual so you can override if you wish
+        protected virtual void NotifyPropertyChanged<TResult>(Expression<Func<TModel, TResult>> property)
+        {
+            // Convert expression to a property name
+            string propertyName = GetPropertyName<TResult>(property);
+
+            // Fire notify property changed event
+            InternalNotifyPropertyChanged(propertyName);
+        }
+
+        private static string GetPropertyName<TResult>(Expression<Func<TModel, TResult>> property)
+        {
+            string propertyName = ((MemberExpression)property.
+                Body).Member.Name;
+            return propertyName;
+        }
+
+        protected void InternalNotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
